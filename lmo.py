85d7d89cd60a4061bce2edeab1bd3d0377dbc735
Felipe <felipe@felipe-OEM.(none)>
2014-07-03 17:30:18 -0300

    Se conecto el codigo a la tabla de monedas nativa de openerp

--- lmo.py
+++ lmo.py
@@ -4,8 +4,8 @@ import datetime
 class lmo_employee(osv.osv):
     _inherit="hr.employee"
     _columns={
-        'employee':fields.one2many('lmo.account', 'employee_id', 'account'),
-        'account_currency_id':fields.many2one('lmo.currency','Account currency'),
+        'employee':fields.one2many('lmo.account', 'employee_id', 'Account'),
+        'account_currency_id':fields.many2one('res.currency','Account currency'),
     }
 
 lmo_employee()
@@ -25,53 +25,54 @@ class lmo_payroll_code(osv.osv):
     _name="lmo.payroll.code"
     _columns={
         'pcode_id':fields.integer('Code',size=225,required=True),
-        'payroll_id':fields.many2one('lmo.payroll.dictionary','Assoc. liquidation'),
-        'pcode_description':fields.char('Description',size=50),
+        'payroll_id':fields.many2one('lmo.payroll.dictionary','Associated liquidation'),
+        'name':fields.char('Description',size=50),
         'pcode_short_descr':fields.char('Short desc.', size=15),
         'pcode_credit_or_debit':fields.selection((('H','Credit'),('R','Retention')),'Credit/Retention',required=True),
-        'pcode_unit_type':fields.selection((('I','Amount'),('%','Percentage'),('U','Unit'),('T','Days/hours/minutes'),('R','Retroactive')),'Unit',required=True),
+        'pcode_unit_type':fields.selection((('I','Amount'),('%','Percentage'),('U','Unit'),('T','Days/Hours/Minutes'),('R','Retroactive')),'Unit',required=True),
         'pcode_autogen':fields.selection((('G','From fixed credits and retentions'),('N','Not generated from credits and retentions')),'Automatic code',required=True),
         'pcode_movement_type':fields.selection((('S','Salary'),('J',"Day's wages"),('C','Common movement')),'Movement type',required=True),
         #'pcode_receipt_type':fields.char('Line type', size=1,required=True),
         'pcode_bases':fields.one2many('lmo.payroll.code.basis','pcode_id','Associated base'),
-        'pcode_taxbases':fields.one2many('lmo.payroll.code.taxbase','pcode_id','Associated tax bases'),
-        'pcode_taxapplications':fields.one2many('lmo.payroll.code.taxapplication','pcode_id','Associated tax applications')
+        'pcode_taxbases_ids':fields.one2many('lmo.payroll.code.taxbase','pcode_id','Associated tax bases'),
+        #'pcode_taxbases_ids':fields.many2many('lmo.payroll.code.taxbase','lmo_payroll_code_taxbase','pcode_id','taxbase_id','Associated tax bases'),
+        'pcode_taxapplications_ids':fields.one2many('lmo.payroll.code.taxapplication','pcode_id','Associated tax applications')
     }
+
     def name_get(self, cr, uid, ids, context=None):
         if not ids:
             return []
-        reads = self.read(cr, uid, ids, ['payroll_id','pcode_id','pcode_short_descr'], context=context)
+        reads = self.read(cr, uid, ids, ['pcode_id','name'], context=context)
         res = []
         for record in reads:
-            #in case there is no description, don't use it or type error will pop
-            description = record['pcode_short_descr']
-            if not description:
-                description = 'No descr'
+            res.append((record['id'],str(record['pcode_id']) + u' - ' + record['name']))
+        return res
 
-                res.append((record['id'],u'' + u' - ' + str(record['pcode_id']) + u' - ' + description))
-            return res
     _sql_constraints=[('unique_code','unique(pcode_id,payroll_id)',"It is not possible to have two codes with the same identifier number on the same liquidation.")]
 
 lmo_payroll_code()
 
 class payroll_dictionary(osv.osv):
+
     _description="Liquidation"
     _name="lmo.payroll.dictionary"
+
     def name_get(self, cr, uid, ids, context=None):
         if not ids:
             return []
         #import pdb
         #pdb.set_trace()
-        reads = self.read(cr, uid, ids, [str('payroll_id'),'payroll_date','company_id'], context=context)
+        reads = self.read(cr, uid, ids, ['payroll_description','payroll_date','company_id'], context=context)
         res = []
         for record in reads:
-            res.append((record['id'], 'Liquidation: ' + str(record['payroll_id']) + '-' + record['company_id'][1] + ' - ' + record['payroll_date']))
+            res.append((record['id'], record['payroll_description'] + ' - ' + record['company_id'][1] + ' - ' + record['payroll_date']))
         return res
+
     _columns={
-        'payroll_currency':fields.many2one('lmo.currency', 'Currency'),
-        'company_id':fields.many2one('res.company','Assoc. company',required=True),
-        'payroll_code':fields.one2many('lmo.payroll.code','payroll_id','Assoc. codes'),
-        'payroll_id':fields.integer('Liquidation id',size=5,required=True),
+        'payroll_currency':fields.many2one('res.currency', 'Currency', required=True),
+        'company_id':fields.many2one('res.company','Associated company',required=True),
+        'payroll_code_ids':fields.one2many('lmo.payroll.code','payroll_id','Associated codes'),
+        ###'payroll_id':fields.integer('Liquidation id',size=5,required=True),
         'payroll_date':fields.date('Date',required=True),
         'payroll_description':fields.char('Liquidation desc.',size=30,required=True),
         'payroll_particular':fields.selection((('T','Total'),('P','Particular')),'Particular liquidation',required=True),
@@ -79,8 +80,11 @@ class payroll_dictionary(osv.osv):
         'payroll_code_type':fields.selection((('T','Total'),('C','Code group')),'Liquidation by code',required=True)
 
     }
-
-    _sql_constraints=[('unique_liquidacion','unique(payroll_id)','The liquidation identifier must be unique!')]
+    
+    
+    _defaults = {
+		'payroll_currency':47,
+        }
 
 payroll_dictionary()
 
@@ -93,23 +97,31 @@ class code_basis(osv.osv):
         'basis_unit_value':fields.float('Unit value',digits=(11,4)),
         'basis_minimum':fields.float('Minimum',digits=(11,4)),
         'basis_exceeds':fields.float('Exceeds',digits=(11,4)),
-        'basis_cap':fields.float('Minimum',digits=(11,4)),
+        'basis_cap':fields.float('Cap',digits=(11,4)),
         'basis_day':fields.float('Basis day',digits=(11,4)),
         'basis_hour':fields.float('Basis hour',digits=(11,4)),
         'basis_computation_type':fields.selection((('+','Addition'),('-','Substraction'),('*','Multiplication'),('/','Division')),'Computation type'),
-        'help':fields.text('Desc', help="Se deben ingresar los datos necesarios para calcular los codigos en tiempo y unidades", required=True, readonly=True),
+        #'help':fields.text('Desc', help="Se deben ingresar los datos necesarios para calcular los codigos en tiempo y unidades", required=True, readonly=True),
     }
 
 code_basis()
 
-class tax_base(osv.osv):
+class lmo_payroll_code_taxbase(osv.osv):
     _description="Taxbase"
     _name="lmo.payroll.code.taxbase"
+
     _columns={
-        'pcode_id':fields.many2one('lmo.payroll.code','Associated code'),
-        'taxbase_id':fields.integer('Taxbase identifier',size=5,required=True),
+        #'pcode_id':fields.many2one('lmo.payroll.code','Associated code'),
+        'pcode_id':fields.integer('Original code'),
+        'taxbase_id':fields.many2one('lmo.payroll.code','Associated code'),
         'taxbase_percentage':fields.float('Percentage',digits=(3,2),required=True)
     }
+
+    _defaults = {
+		'taxbase_percentage': 100,
+    }
+
+lmo_payroll_code_taxbase()
 
 class tax_application(osv.osv):
     _description="Application"
@@ -119,11 +131,14 @@ class tax_application(osv.osv):
         'application_line':fields.integer('Line',size=3,required=True),
         'application_amount_from':fields.float('Amount from',digits=(15,2),required=True),
         'application_amount_upto':fields.float('Amount up to',digits=(15,2),required=True),
-        'application_employee_contrib':fields.float('% employee',digits=(15,2),required=True),
-        'application_employer_contrib':fields.float('% patron',digits=(15,2),required=True),
+        'application_employee_contrib':fields.float('% employee',digits=(15,4),required=True),
+        'application_employer_contrib':fields.float('% employer',digits=(15,4),required=True),
         'application_amount_cap':fields.float('Application amount cap',digits=(15,2),required=True),
         'application_progressive':fields.selection((('O','Progressive'),('L','Progressional')),'Progressive/Progressional',required=True)
     }
+
+tax_application()
+
 
 class lmo_credits_and_deductions_fixed(osv.osv):
     _description="Fixed credits and deductions"
@@ -131,16 +146,54 @@ class lmo_credits_and_deductions_fixed(osv.osv):
     _columns= {
         'payroll_id':fields.many2one('lmo.payroll.dictionary', 'Liquidation', size=5, required=True),
         'employee_id':fields.many2one('hr.employee', 'Employee', size=10, required=True),
-        'section_id':fields.many2one('hr.section','Section', required=True),
         'code_id':fields.many2one('lmo.payroll.code','Code',required=True),
-        'place_of_payment':fields.char('Place of payment', required=True),
         'unit':fields.integer('Units',size=5),
         'day':fields.float('Days'),
         'hour':fields.float('Hours'),
         'minit':fields.integer('Minutes',),
         'unit_amount':fields.float('Unit amount',digits=(15,2)),
         'total_amount':fields.float('Total amount',digits=(15,2)),
+        'category':fields.char('Job name'),
+        'department_id':fields.many2one('hr.department', 'Department'),
+        'pay_type':fields.char('Type of work', size=1),
+        'company_id':fields.integer('Company'),
+        'currency_id':fields.many2one('res.currency', 'Currency'),
+        
+        
+        
+
     }
+    def create(self, cr, uid, values, context=None):
+        #import pdb
+        #pdb.set_trace()
+        #departamento
+        employee_data = self.pool.get('hr.employee').read(cr, uid, values['employee_id'])
+        #compania
+        resource_data = self.pool.get('resource.resource').read(cr, uid, employee_data['resource_id'][0])
+        #moneda
+        currency_data = self.pool.get('lmo.payroll.dictionary').read(cr, uid, values['payroll_id'])
+            
+        
+        values['department_id'] = employee_data['department_id'][0]
+        values['pay_type'] = employee_data['pay_type']
+        values['company_id'] = resource_data['company_id'][0]
+        values['currency_id'] = currency_data['payroll_currency'][0]
+        return super(lmo_credits_and_deductions_fixed, self).create(cr, uid, values, context=context)
+        
+        
+    def write(self, cr, uid, ids, values, context=None):
+        #import pdb
+        #pdb.set_trace()
+        if  'employee_id' in values.keys():
+            employee_data = self.pool.get('hr.employee').read(cr, uid, values['employee_id'])
+            values['department_id'] = employee_data['department_id'][0]
+            values['pay_type'] = employee_data['pay_type'][0]
+            values['company_id'] = resource_data['company_id'][0]
+            values['currency_id'] = currency_data['payroll_currency'][0]
+
+        return super(lmo_credits_and_deductions_fixed, self).write(cr, uid, ids, values, context=context)
+        
+
 
 lmo_credits_and_deductions_fixed()
 
@@ -150,8 +203,6 @@ class lmo_credits_and_deductions_news(osv.osv):
     _columns={
         'payroll_id':fields.many2one('lmo.payroll.dictionary', 'Liquidation', size=5, required=True),
         'employee_id':fields.many2one('hr.employee', 'Employee', size=10, required=True),
-        'section_id':fields.many2one('hr.section','Section', required=True),
-        'place_of_payment':fields.char('Place of payment'),
         'code_id':fields.many2one('lmo.payroll.code','Code', required=True),
         'unit':fields.integer('Units',size=5),
         'day':fields.float('Days'),
@@ -159,7 +210,43 @@ class lmo_credits_and_deductions_news(osv.osv):
         'minit':fields.integer('Minutes'),
         'unit_amount':fields.float('Unit amount',digits=(15,2)),
         'total_amount':fields.float('Total amount',digits=(15,2)),
+        'category':fields.char('Job name'),
+        'department_id':fields.many2one('hr.department', 'Department'),
+        'pay_type':fields.char('Type of work', size=1),
+        'company_id':fields.integer('Company'),
+        'currency_id':fields.many2one('res.currency', 'Currency'),
+        
     }
+    def create(self, cr, uid, values, context=None):
+        #import pdb
+        #pdb.set_trace()
+        #departamento
+        employee_data = self.pool.get('hr.employee').read(cr, uid, values['employee_id'])
+        #compania
+        resource_data = self.pool.get('resource.resource').read(cr, uid, employee_data['resource_id'][0])
+        #moneda
+        currency_data = self.pool.get('lmo.payroll.dictionary').read(cr, uid, values['payroll_id'])
+            
+        
+        values['department_id'] = employee_data['department_id'][0]
+        values['pay_type'] = employee_data['pay_type']
+        values['company_id'] = resource_data['company_id'][0]
+        values['currency_id'] = currency_data['payroll_currency'][0]
+        return super(lmo_credits_and_deductions_news, self).create(cr, uid, values, context=context)
+        
+        
+    def write(self, cr, uid, ids, values, context=None):
+        #import pdb
+        #pdb.set_trace()
+        if  'employee_id' in values.keys():
+            employee_data = self.pool.get('hr.employee').read(cr, uid, values['employee_id'])
+            values['department_id'] = employee_data['department_id'][0]
+            values['pay_type'] = employee_data['pay_type'][0]
+            values['company_id'] = resource_data['company_id'][0]
+            values['currency_id'] = currency_data['payroll_currency'][0]
+
+        return super(lmo_credits_and_deductions_news, self).write(cr, uid, ids, values, context=context)
+
 
 lmo_credits_and_deductions_news()
 
@@ -169,8 +256,6 @@ class lmo_credits_and_deductions_liq(osv.osv):
     _columns={
         'payroll_id':fields.many2one('lmo.payroll.dictionary', 'Liquidation', size=5, required=True),
         'employee_id':fields.many2one('hr.employee', 'Employee', size=10, required=True),
-        'section_id':fields.many2one('hr.section', 'Section', required=True),
-        'place_of_payment':fields.char('Place of payment'),
         'code_id':fields.many2one('lmo.payroll.code', 'Code', required=True),
         'unit':fields.integer('Units', size=5),
         'day':fields.float('Days'),
@@ -180,69 +265,26 @@ class lmo_credits_and_deductions_liq(osv.osv):
         'total_amount':fields.float('Total amount', digits=(15,2)),
         'employee_contrib':fields.float('Personal taxes', digits=(15,2)),
         'employer_contrib':fields.float('Employer taxes', digits=(15,2)),
+        'category':fields.char('Job name'),
+        'department_id':fields.many2one('hr.department', 'Department'),
+        'pay_type':fields.char('Type of work', size=1),
+        'company_id':fields.integer('Company'),
+        'currency_id':fields.many2one('res.currency', 'Currency'),
+
     }
 
 lmo_credits_and_deductions_liq()
 
-# give a meaningful definition of rounding code ACA ES DONDE VOY AGREGAR LO QUE ME PIDE LA DOCUMENTACION
-class lmo_currency(osv.osv):
-    _description="Currency"
-    _name="lmo.currency"
-    def name_get(self, cr, uid, ids, context=None):
-        if not ids:
-            return []
-        reads = self.read(cr, uid, ids, ['currency_code','currency_name'], context=context)
-        res = []
-        for record in reads:
-            res.append((record['id'],'' + record['currency_code'] + ' - ' + record['currency_name']))
-        return res
-    _columns= {
-        'currency_name':fields.char('Name',required=True),
-        'currency_code':fields.char('Currency code',required=True,size=3),
-        'currency_related_groups':fields.one2many('lmo.currency.group', 'currency_id','Associated bill groups'),
-        'round_liquid_code':fields.many2one('lmo.payroll.code', 'Codigo de redoneo de liquido'),
-    }
-
-lmo_currency()
-
-class lmo_currency_group(osv.osv):
-    _description="Currency groups"
-    _name="lmo.currency.group"
-    _columns={
-        'currency_id':fields.many2one("lmo.currency","Associated currency"),
-        'currency_rounding_pay':fields.float('Credits rounding', digits=(5,2), required=True),
-        'currency_rounding_deductions':fields.float('Deductions rounding', digits=(5,2), required=True),
-        'currency_date_start':fields.date("Valid from",required=True),
-        'currency_date_finish':fields.date("Valid up to", required=True),
-        'currency_bills':fields.one2many("lmo.bill","currency_group_id","Associated bills"),
-        'currency_current':fields.boolean("Enabled"),
-    }
-
-
-lmo_currency_group()
-
-class lmo_bill(osv.osv):
-    _description="Billete"
-    _name="lmo.bill"
-    _columns={
-        'currency_group_id':fields.many2one('lmo.currency.group','Group associated'),
-        'bill_id':fields.float('Bill type', digits=(7,2), required=True),
-        'bill_description':fields.char('Bill description', required=True),
-    }
-
-lmo_bill()
-
-
 
 class lmo_account (osv.osv):
-    _description="Contabilizacion"
+    _description="Account"
     _name="lmo.account"
     _columns={
         'liquidation_id':fields.many2one('lmo.payroll.dictionary', 'Liquidation', size=5),
         'accounting_records_id':fields.char('Accountig records', size=5),
         'code_id':fields.many2one('lmo.payroll.code', 'Code' , size=5),
         'active_inactive':fields.selection((('A','Active'),('I','Inactive')),'Active/Inactive'),
-        'department_id':fields.many2one('hr.department','department'),
+        'department_id':fields.many2one('hr.department','Department'),
         'section_id':fields.many2one('hr.section','Section',size=5),
         'local_id':fields.many2one('hr.locals', 'Local', size=5),
         'mtss_id':fields.integer('Mtss', size=2),
@@ -255,3 +297,13 @@ class lmo_account (osv.osv):
     }
 
 lmo_account()
+
+class res_currency(osv.osv):
+
+    _inherit = 'res.currency'
+    _columns = {
+        'currency_code':fields.integer('Local currency code',size=3)
+    }
+
+res_currency()
+
